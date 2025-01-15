09ceed0ec4070cf68d12456eed7e1c38b2dfc1ea
PPekKunGzDev <kittipat25er@gmail.com>
2025-01-16 06:36:46 +0700

    update

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,9 +1,10 @@
 
 export default function Footer() {
+    var date = new Date();
     return (
         <footer className={`w-screen text-white py-4 text-center text-sm border-t border-white/20`}>
             <p>
-                © 2024 <span className="text-orange-400">เว็ปไซต์เก็บพอร์ตของ @PPekKunGzDev</span>. All rights reserved.
+                © 2024-{date.getFullYear()} <span className="text-orange-400">เว็ปไซต์เก็บพอร์ตของ @PPekKunGzDev</span>. All rights reserved.
             </p>
         </footer>
     )
